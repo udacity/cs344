fb3b93c9ea819e728d4de18b4c21e4c2c53c2881
macias <bluedzins@wp.pl>
2013-03-07 23:22:54 +0100

    Update main.cu
    Added const in computeHistogram declaration for consistency and added conditionals to be able to run it on Windows and/or Linux.

--- HW5/main.cu
+++ HW5/main.cu
@@ -5,13 +5,17 @@
 #include "utils.h"
 #include "timer.h"
 #include <cstdio>
+#if defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#else
 #include <sys/time.h>
+#endif
 
 #include <thrust/random/linear_congruential_engine.h>
 #include <thrust/random/normal_distribution.h>
 #include <thrust/random/uniform_int_distribution.h>
 
-void computeHistogram(const unsigned int* d_vals,
+void computeHistogram(const unsigned int *const d_vals,
                       unsigned int* const d_histo,
                       const unsigned int numBins,
                       const unsigned int numElems);
@@ -30,17 +34,21 @@ int main(int argc, char **argv) {
   unsigned int *vals = new unsigned int[numElems];
   unsigned int *histo = new unsigned int[numBins];
 
+#if defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
+  srand(GetTickCount());
+#else
   timeval tv;
   gettimeofday(&tv, NULL);
 
   srand(tv.tv_usec);
+#endif
 
   //make the mean unpredictable, but close enough to the middle
   //so that timings are unaffected
   unsigned int mean = rand() % 100 + 462;
 
   //Output mean so that grading can happen with the same inputs
-  std::cout << "e57__MEAN__f82 " << mean << std::endl;
+  std::cout << mean << std::endl;
 
   thrust::minstd_rand rng;
 
@@ -63,7 +71,7 @@ int main(int argc, char **argv) {
   timer.Start();
   computeHistogram(d_vals, d_histo, numBins, numElems);
   timer.Stop();
-  int err = printf("e57__TIMING__f82 %f msecs.\n", timer.Elapsed());
+  int err = printf("%f msecs.\n", timer.Elapsed());
 
   if (err < 0) {
     //Couldn't print! Probably the student closed stdout - bad news
