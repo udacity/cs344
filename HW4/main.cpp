7cd0cf073ae7220f17fcdf54f92d7e6aa32a0f14
Mike Sarahan <msarahan@gmail.com>
2013-03-22 17:15:09 -0700

    Update main.cpp
    Made timing info output nicer.

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, char **argv) {
   timer.Stop();
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
   printf("\n");
-  int err = printf("%f msecs.\n", timer.Elapsed());
+  int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
 
   if (err < 0) {
     //Couldn't print! Probably the student closed stdout - bad news
