7c7e73e7d8bdf5ffb27989b6eab0508198ae4cad
Qiongsi Wu <wuqiongs@mel-15.syslab.sandbox>
2018-08-13 12:46:57 -0400

    PS4 finishing up

--- Problem Sets/Problem Set 4/student_func.cu
+++ Problem Sets/Problem Set 4/student_func.cu
@@ -217,111 +217,10 @@ void swap_device_ptr(unsigned int **d1, unsigned int **d2) {
 void your_sort(unsigned int *const d_inputVals, unsigned int *const d_inputPos,
         unsigned int *const d_outputVals,
         unsigned int *const d_outputPos, const size_t numElems) {
-    // Temporary testing area...
-    printf("numElems %lu\n", numElems); 
-    // Setting up data
-    size_t testSize = 512;
-    size_t testMemSize = testSize * sizeof(unsigned int);
-    unsigned int *h_test_in = (unsigned int *)malloc(testMemSize);
-    unsigned int *h_test_out = (unsigned int *)malloc(testMemSize);
-    for (unsigned int i = 0; i < testSize; i++) {
-        h_test_in[i] = 1;
-    }
-    // CPU reference result
-    h_test_out[0] = 0;
-    for (unsigned int i = 1; i < testSize; i++) {
-        h_test_out[i] = h_test_in[i - 1] + h_test_out[i - 1];
-    }
-
-    // GPU test
-    unsigned int *d_test_in;
-    unsigned int *d_test_out;
-    unsigned int *h_gpu_out;
-    checkCudaErrors(cudaMalloc((void **)&d_test_in, testMemSize));
-    checkCudaErrors(cudaMalloc((void **)&d_test_out, testMemSize));
-    h_gpu_out = (unsigned int *)malloc(testMemSize);
-
-    checkCudaErrors(
-            cudaMemcpy(d_test_in, h_test_in, testMemSize, cudaMemcpyHostToDevice));
-
-    /*naive_scan<<<1, 8, 8 * sizeof(unsigned int)>>>(d_test_out, d_test_in,
-            testSize);*/
-
-    multiBlockScan(d_test_out, d_test_in, testSize); 
-
-    checkCudaErrors(
-            cudaMemcpy(h_gpu_out, d_test_out, testMemSize, cudaMemcpyDeviceToHost));
-
-    for (unsigned int i = 0; i < testSize; i++) {
-        unsigned int gpu = h_gpu_out[i];
-        unsigned int cpu = h_test_out[i];
-        if (gpu != cpu) {
-            printf("multiblock scan gpu[%u]:%u\t cpu[%u]%u\n", i, gpu, i, cpu);
-        }
-    }
-
-    unsigned int *d_intermediate; 
-    checkCudaErrors(cudaMalloc((void **)&d_intermediate, 4 * sizeof(unsigned int))); 
-    //naive_inclusive_scan_per_block<<<4, 2, sizeof(unsigned int) * 2>>>
-    //    (d_test_out, d_intermediate, d_test_in, testSize); 
-
-    // Testing reordering
-    /*map_to_binFlags<<<gridSize, blockSize>>>(d_binFlags, d_vals_src, 
-                                             mask, i, j, numElems); 
-    
-    multiBlockScan(d_binAddress, d_binFlags, numElems); 
-    
-    offset_by_base<<<gridSize, blockSize>>>(d_binAddress, d_binScan, 
-            j, numElems);
-    
-    reorder<<<gridSize, blockSize>>>(d_vals_dst, d_vals_src, 
-            d_binFlags, d_binAddress, numElems); 
-    
-    reorder<<<gridSize, blockSize>>>(d_pos_dst, d_pos_src, 
-            d_binFlags, d_binAddress, numElems); 
-    
-    // reset d_binFlags after processing for the current
-    // bin. 
-    checkCudaErrors(cudaMemset(d_binFlags, 0, memSize)); 
-    checkCudaErrors(cudaMemset(d_binAddress, 0, memSize)); */
-
-    // cleaning up
-    free(h_test_in);
-    free(h_test_out);
-    free(h_gpu_out);
-    checkCudaErrors(cudaFree(d_test_in));
-    checkCudaErrors(cudaFree(d_test_out));
-
-    // end of testing area...
-
-    const int numBits = 4;
+    const int numBits = 2;
     const int numBins = 1 << numBits;
 
     size_t memSize = sizeof(unsigned int) * numElems;
-
-    // CPU code for testing
-    unsigned int *binHistogram = new unsigned int[numBins];
-    unsigned int *binScan = new unsigned int[numBins];
-
-    unsigned int *vals_src = new unsigned int[numElems];
-    unsigned int *pos_src = new unsigned int[numElems];
-
-    unsigned int *vals_dst = new unsigned int[numElems];
-    unsigned int *pos_dst = new unsigned int[numElems];
-
-    unsigned int *binFlags = new unsigned int[numElems]; 
-    unsigned int *binAddress = new unsigned int[numElems]; 
-    
-    checkCudaErrors(
-            cudaMemcpy(vals_src, d_inputVals, memSize, cudaMemcpyDeviceToHost));
-    checkCudaErrors(
-            cudaMemcpy(pos_src, d_inputPos, memSize, cudaMemcpyDeviceToHost));
-
-    // Temporary CPU arrays
-    unsigned int *h_binHistogram = new unsigned int[numBins];
-    unsigned int *h_binScan = new unsigned int[numBins];
-    unsigned int *h_binFlags = new unsigned int[numElems]; 
-    unsigned int *h_binAddress = new unsigned int[numElems]; 
 
     // GPU arrays
     unsigned int *d_binHistogram;
@@ -351,15 +250,7 @@ void your_sort(unsigned int *const d_inputVals, unsigned int *const d_inputPos,
     // multiples of 2
     // main loop
     for (unsigned int i = 0; i < 8 * sizeof(unsigned int); i += numBits) {
-        printf("XXXXXXXXXX mask at pos %u XXXXXXXXXXX\n", i); 
         unsigned int mask = (numBins - 1) << i;
-
-        memset(binHistogram, 0,
-                sizeof(unsigned int) * numBins);  // zero out the bins
-        memset(binScan, 0, sizeof(unsigned int) * numBins);  // zero out the
-        // bins
-        memset(binFlags, 0, memSize); 
-        memset(binAddress, 0, memSize); 
 
         // Reset GPU arrays
         checkCudaErrors(cudaMemset(d_binHistogram, 0, g_memSize));
@@ -367,104 +258,20 @@ void your_sort(unsigned int *const d_inputVals, unsigned int *const d_inputPos,
         checkCudaErrors(cudaMemset(d_binFlags, 0, memSize)); 
         checkCudaErrors(cudaMemset(d_binAddress, 0, memSize)); 
 
-        // CPU histogram
-        // perform histogram of data & mask into bins
-        for (unsigned int j = 0; j < numElems; ++j) {
-            unsigned int bin = (vals_src[j] & mask) >> i;
-            binHistogram[bin]++;
-        }
-
         // GPU histogram
         simple_hist<<<gridSize, blockSize>>>(d_binHistogram, d_vals_src,
                 numElems, mask, i);
-        cudaDeviceSynchronize(); 
-        checkCudaErrors(cudaMemcpy(h_binHistogram, d_binHistogram, g_memSize,
-                    cudaMemcpyDeviceToHost));
-        // check GPU results
-        for (unsigned int j = 0; j < numBins; j++) {
-            if (h_binHistogram[j] != binHistogram[j]) {
-                printf("GPU hist[%d]:%u\tCPU hist[%d]:%u\n", j,
-                        h_binHistogram[j], j, binHistogram[j]);
-            }
-        }
-
-        // CPU scan
-        // perform exclusive prefix sum (scan) on binHistogram to get starting
-        // location for each bin
-        for (unsigned int j = 1; j < numBins; ++j) {
-            binScan[j] = binScan[j - 1] + binHistogram[j - 1];
-        }
 
         // GPU scan
         naive_scan<<<1, numBins, numBins * sizeof(unsigned int)>>>(
                 d_binScan, d_binHistogram, numBins);
 
-        cudaDeviceSynchronize(); 
-        checkCudaErrors(cudaMemcpy(h_binScan, d_binScan,
-                    numBins * sizeof(unsigned int),
-                    cudaMemcpyDeviceToHost));
-        // check GPU results
-        for (unsigned int j = 0; j < numBins; j++) {
-            if (h_binScan[j] != binScan[j]) {
-                printf("GPU scan[%d]:%u\tCPU scan[%d]:%u\n", j,
-                        h_binHistogram[j], j, binHistogram[j]);
-            }
-        }
-
-        // Gather everything into the correct location
-        // need to move vals and positions
-        for (unsigned int j = 0; j < numElems; ++j) {
-            unsigned int bin = (vals_src[j] & mask) >> i;
-            vals_dst[binScan[bin]] = vals_src[j];
-            pos_dst[binScan[bin]] = pos_src[j];
-            binScan[bin]++;
-        }
-
         // GPU computation to remap the elements
         for (unsigned int j = 0; j < numBins; ++j) {
             map_to_binFlags<<<gridSize, blockSize>>>(d_binFlags, d_vals_src, 
                                                      mask, i, j, numElems); 
-
-            cudaDeviceSynchronize(); 
-            for (unsigned k = 0; k < numElems; k++) {
-                unsigned int bin = ((vals_src[k] & mask) >> i);
-                binFlags[k] = (bin == j)? 1 : 0; 
-            }
-
-            cudaMemcpy(h_binFlags, d_binFlags, memSize, cudaMemcpyDeviceToHost);
-
-            for (unsigned k = 0; k < numElems; k++) {
-                unsigned int gpu = h_binFlags[k]; 
-                unsigned int cpu = binFlags[k]; 
-
-                if (gpu != cpu) {
-                    printf("gpu bin flag[%u]:%u\tcpu bin flag[%u]:%u\n", 
-                            k, gpu, k, cpu); 
-                    exit(1); 
-                }
-            }
-            
             multiBlockScan(d_binAddress, d_binFlags, numElems); 
             
-            binAddress[0] = 0; 
-            for (unsigned k = 1; k < numElems; k++) {
-                binAddress[k] = binAddress[k - 1] + binFlags[k - 1];  
-            }
-
-            cudaMemcpy(h_binAddress, d_binAddress, memSize, cudaMemcpyDeviceToHost); 
-
-            for (unsigned k = 0; k < numElems; k++) {
-                unsigned int gpu = h_binAddress[k]; 
-                unsigned int cpu = binAddress[k]; 
-
-                if (gpu != cpu) {
-                    printf("gpu bin addr[%u]:%u\tcpu bin addr[%u]:%u\n", 
-                            k, gpu, k, cpu); 
-                    exit(1); 
-                }
-            }
-
-
             offset_by_base<<<gridSize, blockSize>>>(d_binAddress, d_binScan, 
                     j, numElems);
             
@@ -476,8 +283,6 @@ void your_sort(unsigned int *const d_inputVals, unsigned int *const d_inputPos,
             
             // reset d_binFlags after processing for the current
             // bin. 
-            memset(binFlags, 0, memSize); 
-            memset(binAddress, 0, memSize); 
             checkCudaErrors(cudaMemset(d_binFlags, 0, memSize)); 
             checkCudaErrors(cudaMemset(d_binAddress, 0, memSize)); 
         }
@@ -486,31 +291,16 @@ void your_sort(unsigned int *const d_inputVals, unsigned int *const d_inputPos,
         checkCudaErrors(cudaMemcpy(h_vals_dst, d_vals_dst, 
                     memSize, cudaMemcpyDeviceToHost));
 
-        for (size_t k = 0; k < numElems; k++) {
-            unsigned int gpu = h_vals_dst[k]; 
-            unsigned int cpu = vals_dst[k]; 
-            if (gpu != cpu) {
-                printf("gpu vals_dst[%lu]:%u\tcpu vals_dst[%lu]:%u\n", 
-                       k, gpu, k, cpu); 
-                exit(1); 
-            }
-        }
-
-        // swap the buffers (pointers only)
-        std::swap(vals_dst, vals_src);
-        std::swap(pos_dst, pos_src);
-
         // swap device pointers
         swap_device_ptr(&d_vals_dst, &d_vals_src);
         swap_device_ptr(&d_pos_dst, &d_pos_src); 
     }
 
     // we did an even number of iterations, need to copy from input buffer into
-    // output std::copy(inputVals, inputVals + numElems, outputVals);
-    // std::copy(inputPos, inputPos + numElems, outputPos);
+    // output 
 	checkCudaErrors(cudaMemcpy(d_outputPos, d_inputPos, memSize, cudaMemcpyDeviceToDevice)); 
 	checkCudaErrors(cudaMemcpy(d_outputVals, d_inputVals, memSize, cudaMemcpyDeviceToDevice)); 
 
-    delete[] binHistogram;
-    delete[] binScan;
+	checkCudaErrors(cudaFree(d_binHistogram));
+   	checkCudaErrors(cudaFree(d_binScan)); 
 }
